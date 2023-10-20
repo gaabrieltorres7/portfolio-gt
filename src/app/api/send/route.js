883afc2d30c35f992ce9145e9a76c19d730db2a5
Gabriel Torres <gaabrieltt7@gmail.com>
2023-10-20 13:46:22 -0300

    refactor: api key

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_4xnqp6vz_4eoWi8dYy51jLPPqniZbSPSD");
 const myEmail = process.env.MY_EMAIL;
 
 export async function POST(req, res) {
